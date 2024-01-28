bc5e4ac7df2d538cf6ad4bf13c8c0ee0aa472636
opdo <52803560+netfledgling@users.noreply.github.com>
2024-01-28 00:45:47 -0800

    1/5 pset1

---
+++ coursework/python/pset1.py
@@ -0,0 +1,19 @@
+# deep thought
+answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+if answer == "forty-two" or answer == "forty two" or int(answer) == 42:
+  print("Yes")
+else:
+  print("No")
+
+# home federal savings bank
+greet = input("Greeting: ")
+if greet[:5] == "Hello" or greet[:5] == "hello":
+  print("$0")
+elif greet[0].lower() == "h":
+  print("$20")
+else:
+  print("$100")
+
+# file extensions
+file = input("File name: ")
+ext = 
