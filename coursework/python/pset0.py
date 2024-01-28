4a67dd7ab10f21ac6664e592e58edf8ecde94992
opdo <52803560+netfledgling@users.noreply.github.com>
2024-01-28 00:17:54 -0800

    2/5 pset0

---
+++ coursework/python/pset0.py
@@ -0,0 +1,40 @@
+# indoor
+load = input()
+print(load.lower())
+
+# playback speed
+load = input()
+
+# faces
+def convert(str emoticon):
+  str emoji
+  🙂🙁
+
+def main():
+  state = input()
+  print(convert(state))
+
+main()
+
+# einstein 
+m = int(input("m: "))
+l = 300000000
+print("E:", m*l**2)
+
+# tip 
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
+
+
+def dollars_to_float(d):
+    # TODO
+
+
+def percent_to_float(p):
+    # TODO
+
+
+main()
